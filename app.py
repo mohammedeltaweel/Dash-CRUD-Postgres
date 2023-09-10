5239faa65ed7e7713f7a39c5e46ae671a3da63da
Eltaweel <mohammedeltaweel40@gmail.com>
2023-09-10 07:41:54 +0300

    added the app structure

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 import dash 
-from dash import html, dcc, Input, Output, State, callback
+from dash import Dash, html, dcc, Input, Output, State, callback
 import dash_table
 import pandas as pd
 import plotly.express as px
+
+app = Dash(__name__)
+
+
+app.layout = html.Div([
+    html.Div(className="btn-and-input", children=[
+        dcc.Input(
+            id="column-name", 
+            placeholder="Enter a column name",
+            type="text"),
+        html.Button("Add Column", className="my-btn")
+    ]),
+    dash_table.DataTable(),
+    html.Div([
+        html.Button("Add Row", className="my-btn"),
+        html.Button("Export to Excel", className="my-btn")
+    ])
+])
